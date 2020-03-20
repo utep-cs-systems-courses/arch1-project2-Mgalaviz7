e965d0a937edce1e83780290dba322f8898d4683
Mgalaviz7 <mgalaviz3@miners.utep.edu>
2020-03-20 15:14:46 -0600

    clean up and addition of comments.

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -1,51 +1,119 @@
 #include <msp430.h>
+#include <stdlib.h>
 #include "stateMachines.h"
 #include "led.h"
+#include "switches.h"
+#include "buzzer.h"
 
-char count_to_3()		/* always toggle! */
+static int blink_flag;
+/*
+  This method is always toggled and is used to keep the current state
+  of the state machine. Each state changes due to the time a button is
+  pushed and remains in that state till another button is pushed.
+  Read the README to get a better in depth understanging or each state
+  and button fucnctionality.
+ */
+void
+states()
 {
-  static char state = 0;
-
+  static char state = 0 ;
+  
   switch (state) {
   case 0:
-    red_on = 1;
-    state = 1;
-    break;
-
-  case 2:
-    red_on = 0;
-    state = 0;
+    /* Is currently shown that it is at state 0 and ready to go*/
+    green_led_on();   
+    state = CURRENT_STATE;
     break;
 
   case 1:
-    red_on = 0;
+    blink_flag = 0;
+    count_to_3();
+    break;
+
+  case 2:
+    blink_flag = 1;
+    count_to_3();
+    state = 2;
+    break;
+
+  case 3:
+    state = 3;
+    break;
+
+  case 4:
+    little_lamb();
+    state = 4;
+    break;
+  }
+}
+
+/*
+  This method is always toggled either when button1 or button2 is pressed. 
+  If button 1 is pressed it counts from 0 to the value 3 in binary rersination.
+  By calling the methods in led.c
+  If button 2 is pressed it trigres the dim flag which essentialy dims the counting to
+  3.
+ */
+void
+count_to_3()
+{  
+  static char state = 0;
+  
+  switch (state) {
+  case 0:
+    both_leds_off();
+    state = 1;
+    break;
+
+  case 1:
+    if (blink_flag == 1)
+      {
+	for(int i =0; i<100; i++)
+	  {
+	    red_led_on();
+	    red_led_off();
+	  }
+      }
+    else
+      {
+	red_led_on();
+      }
+    state = 2;
+    break;
+    
+  case 2:
+     if(blink_flag == 1)
+      {
+	for(int i =0; i<100; i++)
+	  {
+	    red_led_off();
+	    green_led_on();
+	    green_led_off();
+	  }
+      }
+     else
+      {
+	red_led_off();
+	green_led_on();
+      }
+     state = 3;
+     break;
+
+  case 3:
+    if(blink_flag == 1)
+      {
+	for(int i =0; i<100; i++)
+	  {
+	    both_leds_on();
+	    both_leds_off();
+	  }
+      }
+    else
+      {
+	both_leds_on();
+      }
     state = 0;
     break;
   }
-  return 1;			/* always changes an led */
-}
-
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
-}
-
-
-void state()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = count_to_3(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
-  }
-
-  led_changed = changed;
   led_update();
 }
