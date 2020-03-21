96a9cdfe0af9915d56c105d44e2fb99e0bea5142
Mgalaviz7 <mgalaviz3@miners.utep.edu>
2020-03-20 22:34:33 -0600

    added comments

--- project/led.c
+++ project/led.c
@@ -5,7 +5,6 @@ unsigned char red_on , green_on =0;
 
 static char redVal[] = {0, LED_RED} , greenVal[] = {0, LED_GREEN};
 
-
 void
 led_init()
 {
@@ -22,6 +21,10 @@ led_update()
 
   P1OUT |= ledFlags;
 }
+
+/* 
+   All these methods are mostly the same and for the most part these methods just turn on and off the leds by setting either green_on or red_on to a 1 or 0.
+ */
 
 void
 green_led_on()
