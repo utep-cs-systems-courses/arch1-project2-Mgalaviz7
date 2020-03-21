e94cafd27b47b62d145ab8547b4acbdbdff2d266
Mgalaviz7 <mgalaviz3@miners.utep.edu>
2020-03-20 22:46:35 -0600

    added comments

--- project/p2_interrupt_handler.c
+++ project/p2_interrupt_handler.c
@@ -3,7 +3,11 @@
 #include "stateMachines.h"
 #include "buzzer.h"
 
-
+/*
+  This method is awating for a switch to be pressed 
+  if there is one the method calls switch_interrupt_handler
+  in switches.c to update the state.
+*/
 void
 __interrupt_vec(PORT2_VECTOR) Port_2(){
   if (P2IFG & SWITCHES) {
@@ -11,7 +15,11 @@ __interrupt_vec(PORT2_VECTOR) Port_2(){
     switch_interrupt_handler();
   }
 }
-
+/*
+  This method is costently being run and controls the 
+  speed at which the lights blink in the state machien
+  as well keep the current state until interrupted by a button.
+*/
 void
 __interrupt_vec(WDT_VECTOR) WDT() {
   static char blink_count =0;  
